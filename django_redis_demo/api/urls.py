47727ea410494a003438d3527c7820a2753fc230
Tom Xu <txu@guidehouse.com>
2021-10-27 19:39:14 -0400

    update readme.md1

--- django_redis_demo/api/urls.py
+++ django_redis_demo/api/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from .views import manage_items, manage_item
 
 urlpatterns = {
-    path('items', manage_items, name="items"),
-    path('items/<slug:key>', manage_item, name="single_item")
+    path('items/', manage_items, name="items"),
+    path('items/<slug:key>/', manage_item, name="single_item")
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- django_redis_demo/django_redis_demo/urls.py
+++ django_redis_demo/django_redis_demo/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.generic import RedirectView
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Redirect to api multiple items view
+    path('', RedirectView.as_view(url='api/items/')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
